d2fc38903937e8d3ef911e90035541d797da57b4
Vikram Dham <vikram@kamboi.com>
2015-10-25 08:08:24 +0000

    hack to bypass incorrect binding of 6640 listener

--- devstack/provisioning/setup-compute.sh
+++ devstack/provisioning/setup-compute.sh
@@ -3,4 +3,4 @@ cp networking-ovn/devstack/computenode-local.conf.sample devstack/local.conf
 if [ "$1" != "" ]; then
     sed -i -e 's/<IP address of host running everything else>/'$1'/g' devstack/local.conf
 fi
-devstack/stack.sh
+#devstack/stack.sh
--- devstack/provisioning/setup-controller.sh
+++ devstack/provisioning/setup-controller.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cp networking-ovn/devstack/local.conf.sample devstack/local.conf
-devstack/stack.sh
+#devstack/stack.sh
